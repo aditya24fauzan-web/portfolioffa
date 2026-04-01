0e7c48097ccc5f3837aacc7de87636088f84a617
aditya24fauzan-web <aditya24fauzan@gmail.com>
2026-04-02 02:34:30 +0700

    Add files via upload

--- php/send.php
+++ php/send.php
@@ -1,1 +1,24 @@
+<?php
 
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])){
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = "fadzillahfauzanaditya4@gmail.com";   // ganti dengan email kamu
+$subject = "Pesan dari Portfolio Website";
+
+$body = "Name: $name\n";
+$body .= "Email: $email\n\n";
+$body .= "Message:\n$message";
+
+$headers = "From: $email";
+
+mail($to,$subject,$body,$headers);
+
+echo "Message sent successfully!";
+
+}
+
+?>
